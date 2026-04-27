95a2a631d79c0c58f77015fa0c5a8ce2cb2c91fb
Ayelen <ayelenvegacaro@gmail.com>
2026-04-27 16:23:44 -0300

    Prueba PLL

--- Prueba de funcionamiento/src/main.c
+++ Prueba de funcionamiento/src/main.c
@@ -1,42 +1,115 @@
 #include "stm32f1xx_hal.h"
 
-// Función para inicializar el PIN del LED
+// --- Definiciones para CLK ---
+#define CLK_PIN   GPIO_PIN_0
+#define CLK_PORT  GPIOA
+
+// --- Definiciones para DATA ---
+#define DATA_PIN  GPIO_PIN_1
+#define DATA_PORT GPIOA
+
+// --- Definiciones para EN ---
+#define EN_PIN    GPIO_PIN_2
+#define EN_PORT   GPIOA
+
+#define FRECUENCY 10000
+
+static uint32_t delay_uS = (1 / (2 * FRECUENCY)) * 10e6;
+
+// Prototipos de funciones
+void SystemClock_Config(void);
+void GPIO_Init(void);
+void Send_19Bits(uint32_t data);
+void Delay_uS(uint32_t us);
+
 void GPIO_Init(void) {
-    // 1. Estructura de configuración
+    // 1. Habilitar el reloj del puerto A
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
     GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-    // 2. Activar el reloj del Puerto C (Donde está el LED PC13)
-    // IMPORTANTE: Sin esto, el puerto no funciona y el programa se cuelga
-    __HAL_RCC_GPIOC_CLK_ENABLE();
-
-    // 3. Configurar el Pin PC13
-    GPIO_InitStruct.Pin = GPIO_PIN_13;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; // Push-Pull
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    // 2. Configuración para el CLK
+    GPIO_InitStruct.Pin = CLK_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(CLK_PORT, &GPIO_InitStruct);
 
-    // 4. Aplicar la configuración
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    // 2. Configuración para el DATA
+    GPIO_InitStruct.Pin = DATA_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(DATA_PORT, &GPIO_InitStruct);
+
+    // 2. Configuración para el EN
+    GPIO_InitStruct.Pin = EN_PIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(EN_PORT, &GPIO_InitStruct);
+}
+void SystemClock_Config(void) {
 }
 
 int main(void) {
-    // 1. Inicializar la librería HAL (Configura SysTick para HAL_Delay, etc.)
-    HAL_Init();
-
-    // 2. Inicializar nuestros periféricos (GPIO)
+    HAL_Init(); // Inicializa la librería HAL
+    SystemClock_Config();
     GPIO_Init();
+    uint32_t miDato = 0x07D06; // N = 250, A = 6 y SW1 = SW2 = 0
+    // 0x7FFFC; todos 1 y dos ceros al final
+    // N = 250, A = 6 y SW1 = SW2 = 0 -> miDato = 0x07D06
+    // SW1 = SW2 = 1 -> miDato = 0x67D06
+    // SW1 = 1 y SW2 = 0 -> miDato = 0x47D06
 
-    // 3. Bucle infinito (El "loop" de Arduino)
     while (1) {
-        // Togglear (invertir) el estado del pin PC13
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-
-        // Esperar 500 milisegundos
-        HAL_Delay(500);
+        Send_19Bits(miDato);
+        HAL_Delay(2000);
     }
 }
 
-// Necesario para que SysTick funcione correctamente en STM32Cube
+void Send_19Bits(uint32_t data) {
+    // 1. Asegurar estado inicial: CLK bajo, EN bajo, DATA en Idle (alto)
+    HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(EN_PORT, EN_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(DATA_PORT, DATA_PIN, GPIO_PIN_SET);
+    HAL_Delay(1);
+
+    // 2. Transmision de los 19 bits
+    for (int i = 18; i >= 0; i--) {
+        // Establecer el bit en DATA antes del flanco de subida
+        if ((data >> i) & 0x01) {
+            HAL_GPIO_WritePin(DATA_PORT, DATA_PIN, GPIO_PIN_SET);
+        } else {
+            HAL_GPIO_WritePin(DATA_PORT, DATA_PIN, GPIO_PIN_RESET);
+        }
+
+        HAL_Delay(1); // Tiempo de establecimiento
+
+        // Flanco ascendente de CLK (El receptor lee aquí)
+        HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_SET);
+        HAL_Delay(1);
+        HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_RESET);
+    }
+
+    // 3. Generar pulso de Enable tras los 19 bits
+    HAL_Delay(1);
+    // HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(EN_PORT, EN_PIN, GPIO_PIN_SET);
+    HAL_Delay(1);
+    // HAL_GPIO_WritePin(CLK_PORT, CLK_PIN, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(EN_PORT, EN_PIN, GPIO_PIN_RESET);
+
+    // 4. Dejar DATA en Idle (1)
+    HAL_GPIO_WritePin(DATA_PORT, DATA_PIN, GPIO_PIN_SET);
+}
+
+void Delay_uS(uint32_t us) {
+    uint32_t count = us * 3;
+    while (count--) {
+        __NOP();
+    }
+}
 void SysTick_Handler(void) {
     HAL_IncTick();
 }
